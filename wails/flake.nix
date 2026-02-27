a8b0ef2740f812b4fdc748b90dfb367d1f7d1d78
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-02-27 12:46:52 +0100

    Added wails

---
+++ wails/flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs.flakelight.url = "github:nix-community/flakelight";
+  outputs = {flakelight, ...}:
+    flakelight ./. {
+      devShell = {
+        packages = pkgs: [pkgs.wails pkgs.go pkgs.npm];
+      };
+    };
+}
