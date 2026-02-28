0eaa3ca7d639a249aa5107d9c5d6e274d5649259
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-02-28 15:43:02 +0100

    Fixed tauri

--- rust/tauri/flake.nix
+++ rust/tauri/flake.nix
@@ -7,13 +7,19 @@
           pkgs.pkg-config
           pkgs.wrapGAppsHook4
           pkgs.cargo
-          pkgs.cargo-tauri
           pkgs.nodejs
           pkgs.librsvg
           pkgs.webkitgtk_4_1
+
+          pkgs.cargo
+          pkgs.rust-analyzer
+          pkgs.rustc
+          pkgs.clippy
+          pkgs.rustfmt
         ];
-        shellHook = ''
+        shellHook = pkgs: ''
           export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH"
+          export RUST_SRC_PATH="${pkgs.rustPlatform.rustLibSrc}";
         '';
       };
     };
