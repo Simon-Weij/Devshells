efd467e1b60c1787eb1dab3d0025437bc0de46c9
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-07 14:51:58 +0100

    Added bun stuff

---
+++ python/python/flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs.flakelight.url = "github:nix-community/flakelight";
+  outputs = {flakelight, ...}:
+    flakelight ./. {
+      devShell = {
+        packages = pkgs: [pkgs.python313];
+      };
+    };
+}
